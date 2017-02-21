b44bc50ae7699c75065419bfcf6047d66379f186
Neil Madden <Neil.E.Madden@gmail.com>
2017-02-21 14:50:17 +0000

    Lock down connect permissions on database

--- scripts/sql/create_database.sql
+++ scripts/sql/create_database.sql
@@ -6,3 +6,9 @@ CREATE ROLE "job_creator" LOGIN PASSWORD 'password';
 CREATE ROLE "dd_api" LOGIN PASSWORD 'password';
 
 CREATE DATABASE "data_discovery" OWNER 'data_discovery';
+
+-- Lock down the database to only allow those users to connect
+REVOKE ALL PRIVILEGES ON DATABASE "data_discovery" FROM PUBLIC;
+GRANT CONNECT ON DATABASE "data_discovery" TO data_discovery;
+GRANT CONNECT ON DATABASE "data_discovery" TO job_creator;
+GRANT CONNECT ON DATABASE "data_discovery" TO dd_api;
